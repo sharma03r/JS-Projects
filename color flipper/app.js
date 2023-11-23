e13de9403690f1ff4c550aab02fc673779a6dbb3
Rahul Sharma <sharmarahul8848@gmail.com>
2023-11-23 19:00:03 +0530

    first commit

---
+++ color flipper/app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
